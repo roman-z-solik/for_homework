7cfcf16ff75eaf0f077611a26a936bd5a7cf6a35
Roman Z <roman-z@inbox.ru>
2025-04-09 16:10:04 +0500

    Add function get_date

--- src/widget_bank/widget.py
+++ src/widget_bank/widget.py
@@ -9,12 +9,8 @@ def mask_account_card(user_payment_method: str) -> str:
     else:
         return(user_payment_method[:-16] + " " + get_mask_card_number(user_payment_method[-16:]))
 
-# print(mask_account_card("Счет 64686473678894779589"))
-# print(mask_account_card("Maestro 1596837868705199"))
-# print(mask_account_card("MasterCard 7158300734726758"))
-# print(mask_account_card("Счет 35383033474447895560"))
-# print(mask_account_card("Visa Classic 6831982476737658"))
-# print(mask_account_card("Visa Platinum 8990922113665229"))
-# print(mask_account_card("Visa Gold 5999414228426353"))
-# print(mask_account_card("Счет 73654108430135874305"))
-#print(get_date("2023-12-23T02:26:18.671407"))
+
+def get_date(user_date: str) -> str:
+    """Функция, которая принимает на вход дату
+       и возвращает её в формате ДД.ММ.ГГГГ"""
+    return(f"{user_date[8:10]}.{user_date[5:7]}.{user_date[:4]}")

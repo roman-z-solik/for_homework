9c5f569d5a1d4a80ea308cb8345c38ed3c7e6c5c
Roman Z <roman-z@inbox.ru>
2025-04-11 11:49:02 +0500

    Add functions filter_by_state and sort_by_date in processing.py

--- src/widget_bank/processing.py
+++ src/widget_bank/processing.py
@@ -11,10 +11,18 @@ def filter_by_state(input_operations: list, state: str = "EXECUTED") -> list:
     state (по умолчанию EXECUTED). Dозвращает новый список словарей,
     содержащий только те словари, у которых ключ state
     соответствует указанному значению."""
-    output_operations = []
+    output_state_operations = []
     for operation in input_operations:
         if operation["state"] == state:
-            output_operations.append(operation)
+            output_state_operations.append(operation)
         else:
             continue
-    return output_operations
+    return output_state_operations
+
+
+def sort_by_date(input_operations: list, reverse_status: bool = True) -> list:
+    """Функция принимает список словарей и необязательный параметр,
+    задающий порядок сортировки (по умолчанию — убывание).
+    Функция должна возвращать новый список, отсортированный по дате"""
+    output_time_operation = sorted(input_operations, key=lambda x: x["date"], reverse=reverse_status)
+    return output_time_operation

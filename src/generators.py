4d9d159c1a9fe4ecaa98cd3f782fec85639eb638
Roman Z <roman-z@inbox.ru>
2025-05-04 13:37:16 +0500

    Make any changes in generators.py

--- src/generators.py
+++ src/generators.py
@@ -36,77 +36,14 @@ def card_number_generator(start: int, stop: int) -> Generator:
         or stop > 10000000000000000
     ):
         raise TypeError("Введите число в диапазоне от 0 до 10000000000000000")
-    for num in generated_num:
-        if num < 10:
-            generated_card_number = card_number[:-1] + str(num)
-            generated_card_numbers.append(generated_card_number)
-        elif num < 100:
-            generated_card_number = card_number[:-2] + str(num)
-            generated_card_numbers.append(generated_card_number)
-        elif num < 1000:
-            generated_card_number = card_number[:-3] + str(num)
-            generated_card_numbers.append(generated_card_number)
-        elif num < 10000:
-            generated_card_number = card_number[:-4] + str(num)
-            generated_card_numbers.append(generated_card_number)
-        elif num < 100000:
-            generated_card_number = card_number[:-6] + str(num)[-5] + " " + str(num)[-4:]
-            generated_card_numbers.append(generated_card_number)
-        elif num < 1000000:
-            generated_card_number = card_number[:-7] + str(num)[-6:-4] + " " + str(num)[-4:]
-            generated_card_numbers.append(generated_card_number)
-        elif num < 10000000:
-            generated_card_number = card_number[:-8] + str(num)[-7:-4] + " " + str(num)[-4:]
-            generated_card_numbers.append(generated_card_number)
-        elif num < 100000000:
-            generated_card_number = card_number[:-9] + str(num)[-8:-4] + " " + str(num)[-4:]
-            generated_card_numbers.append(generated_card_number)
-        elif num < 1000000000:
-            generated_card_number = card_number[:-11] + str(num)[-9] + " " + str(num)[-8:-4] + " " + str(num)[-4:]
-            generated_card_numbers.append(generated_card_number)
-        elif num < 10000000000:
-            generated_card_number = card_number[:-12] + str(num)[-11:-8] + " " + str(num)[-8:-4] + " " + str(num)[-4:]
-            generated_card_numbers.append(generated_card_number)
-        elif num < 100000000000:
-            generated_card_number = card_number[:-13] + str(num)[-12:-8] + " " + str(num)[-8:-4] + " " + str(num)[-4:]
-            generated_card_numbers.append(generated_card_number)
-        elif num < 1000000000000:
-            generated_card_number = card_number[:-14] + str(num)[-13:-8] + " " + str(num)[-8:-4] + " " + str(num)[-4:]
-            generated_card_numbers.append(generated_card_number)
-        elif num < 10000000000000:
-            generated_card_number = (
-                card_number[:3] + str(num)[-13] + " " + str(num)[-12:-8] + " " + str(num)[-8:-4] + " " + str(num)[-4:]
-            )
-            generated_card_numbers.append(generated_card_number)
-        elif num < 100000000000000:
-            generated_card_number = (
-                card_number[:2]
-                + str(num)[-14:-12]
-                + " "
-                + str(num)[-12:-8]
-                + " "
-                + str(num)[-8:-4]
-                + " "
-                + str(num)[-4:]
-            )
-            generated_card_numbers.append(generated_card_number)
-        elif num < 1000000000000000:
-            generated_card_number = (
-                card_number[:1]
-                + str(num)[-15:-12]
-                + " "
-                + str(num)[-12:-8]
-                + " "
-                + str(num)[-8:-4]
-                + " "
-                + str(num)[-4:]
-            )
-            generated_card_numbers.append(generated_card_number)
-        elif num < 10000000000000000:
-            generated_card_number = (
-                str(num)[-16:-12] + " " + str(num)[-12:-8] + " " + str(num)[-8:-4] + " " + str(num)[-4:]
-            )
-            generated_card_numbers.append(generated_card_number)
-
-    for generated_card_number in generated_card_numbers:
-        yield (generated_card_number)
+    while start <= stop:
+        zeros_num = 16 - len(str(start))
+        number_card_no_spaces = "0" * zeros_num + str(start)
+        number_card = (
+            f"{number_card_no_spaces[:4]} "
+            f"{number_card_no_spaces[4:8]} "
+            f"{number_card_no_spaces[8:12]} "
+            f"{number_card_no_spaces[12:]}"
+        )
+        yield number_card
+        start += 1

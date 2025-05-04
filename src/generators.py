364bd036d5513318a07cf1d3f71e861f5466049d
Roman Z <roman-z@inbox.ru>
2025-05-04 16:52:54 +0500

    Making changes in README.md and generators.py

--- src/generators.py
+++ src/generators.py
@@ -7,14 +7,14 @@ def filter_by_currency(transactions: dict, currency: str = "USD") -> Generator[D
     соответствует заданной (например, USD)."""
     for transaction in transactions:
         if transaction["operationAmount"]["currency"]["code"] == currency:
-            yield (transaction)
+            yield transaction
 
 
 def transaction_descriptions(transactions: dict):  # type: ignore[no-untyped-def]
     """Генератор transaction_descriptions, который принимает список словарей
     с транзакциями и возвращает описание каждой операции по очереди."""
     for transaction in transactions:
-        yield (transaction["description"])
+        yield transaction["description"]
 
 
 def card_number_generator(start: int, stop: int) -> Generator:
@@ -22,11 +22,6 @@ def card_number_generator(start: int, stop: int) -> Generator:
     номера банковских карт в формате XXXX XXXX XXXX XXXX, где
     X — цифра номера карты. Генератор может сгенерировать номера карт
     в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999"""
-    gen_numbers = range(start, stop)
-    generated_num = [x for x in gen_numbers]
-    card_number = "0000 0000 0000 0000"
-    generated_card_number = ""
-    generated_card_numbers = []
     if (
         not str(start).isdigit()
         or not str(stop).isdigit()

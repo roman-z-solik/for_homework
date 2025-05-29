96381d167db7a45e12845b690d98a6ac53e84ff5
Roman Z <roman-z@inbox.ru>
2025-05-29 17:08:22 +0500

    Make any changes in interface.py and main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from pathlib import Path
-from src.interface import select_file_type, sort, sort_re, user_input_state, user_input_y_n, user_input_search
-
-
+from src.interface import select_file_type, sort, sort_re, user_input_state, user_input_y_n, user_input_search, count_categories, result_output
 
 root_path = Path(__file__).resolve().parents[1]
-
 
 if __name__ == "__main__":
     print("Привет! Добро пожаловать в программу работы с банковскими транзакциями.")
     transact = select_file_type()
-    print(f"Введите статус, по которому необходимо выполнить фильтрацию."
-          f"\nДоступные для фильтровки статусы: [E]XECUTED, [C]ANCELED, [P]ENDING.\n")
+    print(
+        "Введите статус, по которому необходимо выполнить фильтрацию."
+        "\nДоступные для фильтровки статусы: [E]XECUTED, [C]ANCELED, [P]ENDING.\n"
+    )
     user_state = user_input_state()
     transact = sort_re(transact, user_state, 'state')
     print("Отсортировать операции по дате? [Д]а/[Н]ет\n")
@@ -20,10 +19,22 @@ if __name__ == "__main__":
     transact = sort(transact, "date", user_reverse, yes_no)
     print("\nВыводить только рублевые транзакции? [Д]а/[Н]ет\n")
     yes_no = user_input_y_n()
-    if yes_no == True:
+    if yes_no:
         transact = sort_re(transact, "RUB", "currency_code")
     print("\nОтфильтровать список транзакций по определенному слову в описании? [Д]а/[Н]ет\n")
     yes_no = user_input_y_n()
     transact = user_input_search(transact, yes_no)
-    for transaction in transact:
-        print(transaction)
+    print("\nРаспечатываю итоговый список транзакций...\n")
+    count = count_categories(transact)
+    if count == 0:
+        print("\nНе найдено ни одной транзакции, \nподходящей под ваши условия фильтрации")
+    else:
+        print(f"Всего банковских операций в выборке: {count}")
+        result = result_output(transact)
+        print(result)
+
+    # if transact != []:
+    #     for transaction in transact:
+    #         print(transaction)
+    # else:
+    #     print("\nНе найдено ни одной транзакции, \nподходящей под ваши условия фильтрации")
--- src/interface.py
+++ src/interface.py
@@ -2,8 +2,10 @@ import logging
 import re
 from pathlib import Path
 
+
+from src.masks import get_mask_account
+from src.widget import get_date
 from src.utils import read_csv_file, read_excel_file, read_json_file
-from tests.conftest import transactions
 
 root_path = Path(__file__).resolve().parents[1]
 logging.basicConfig(
@@ -26,7 +28,7 @@ def select_file_type() -> list:
     while True:
         type_input = input(f"{text}\nВвод: ")
         if type_input == "1":
-            print(f"\nДля обработки выбран JSON файл.\n")
+            print("\nДля обработки выбран JSON файл.\n")
             json_read_file = read_json_file(f"{root_path}/data/operations.json")
             return json_read_file
             break
@@ -39,7 +41,7 @@ def select_file_type() -> list:
             return read_excel_file(f"{root_path}/data/transactions_excel.xlsx")
             break
         else:
-            print(f"\nВыбран неверный пункт. Повторите ввод.")
+            print("\nВыбран неверный пункт. Повторите ввод.")
 
 
 def user_input_state() -> str:
@@ -48,23 +50,23 @@ def user_input_state() -> str:
         if user_input.lower() == "executed" or user_input.lower() == "e" or user_input.lower() == "у":
             user_state = "EXECUTED"
             print(f"\nОперации отфильтрованы по статусу {user_state}\n")
-            return(user_state)
+            return user_state
             break
         elif user_input.lower() == "canceled" or user_input.lower() == "c" or user_input.lower() == "с":
             user_state = "CANCELED"
             print(f"\nОперации отфильтрованы по статусу {user_state}\n")
-            return (user_state)
+            return user_state
             break
         elif user_input.lower() == "pending" or user_input.lower() == "p" or user_input.lower() == "з":
             user_state = "PENDING"
             print(f"\nОперации отфильтрованы по статусу {user_state}\n")
-            return (user_state)
+            return user_state
             break
         else:
-            print(f"\nСтатус операции \"{user_input}\" недоступен. Попробуйте ввести заново.\n")
+            print(f'\nСтатус операции "{user_input}" недоступен. Попробуйте ввести заново.\n')
 
 
-def sort_re(transactions: list, sorted_value:str, sorted_item:str) -> list:
+def sort_re(transactions: list, sorted_value: str, sorted_item: str) -> list:
     """Функция принимает список словарей с данными о банковских операциях и строку поиска,
     после чего возвращает список словарей, у которых в описании есть данная строка"""
     sorted_transactions_state = []
@@ -72,37 +74,69 @@ def sort_re(transactions: list, sorted_value:str, sorted_item:str) -> list:
         try:
             if re.findall(str(sorted_value), item[sorted_item], flags=re.IGNORECASE):
                 sorted_transactions_state.append(item)
-        except (TypeError,KeyError) as e:
+        except (TypeError, KeyError) as e:
             logger_util.info(f"{e} ошибка")
-    return (sorted_transactions_state)
+    return sorted_transactions_state
 
 
-def sort(transactions: list, key_value:str, reverse:bool, status:bool) -> list:
+def sort(transactions: list, key_value: str, reverse: bool, status: bool) -> list:
     """Функция принимает список словарей с данными о банковских операциях, строку для сортировки,
-        строку статуса для реверса сортировки, сортирует список, после чего возвращает
-        список словарей"""
-    if status == True:
-        sorted_transactions = sorted(transactions, key=lambda transaction: transaction[key_value], reverse = reverse)
-        return (sorted_transactions)
+    строку статуса для реверса сортировки, сортирует список, после чего возвращает
+    список словарей"""
+    if status:
+        sorted_transactions = sorted(transactions, key=lambda transaction: transaction[key_value], reverse=reverse)
+        return sorted_transactions
     else:
-        return(transactions)
+        return transactions
 
 
 def user_input_y_n() -> bool:
     """Функция, которая отпределяет, оветил пользователь ДА или НЕТ на вопрос"""
-    user_input = input("Ввод: ")
-    if user_input.lower() == "lf" or user_input.lower() == "да" or user_input.lower() == "l" or user_input.lower() == "д":
-        return(True)
-    else:
-        return(False)
+    while True:
+        user_input = input("Ввод: ")
+        if (
+            user_input.lower() == "lf"
+            or user_input.lower() == "да"
+            or user_input.lower() == "l"
+            or user_input.lower() == "д"
+        ):
+            return True
+        elif (
+                user_input.lower() == "ytn"
+                or user_input.lower() == "нет"
+                or user_input.lower() == "y"
+                or user_input.lower() == "н"
+        ):
+            return False
+        else:
+            print("\nВведите [Д]а или [Н]ет\n")
 
 
-def user_input_search(transactions, yes_no):
-    if yes_no == True:
-        search_value = input("\nВведите строку для поиска: ")
-        print(search_value)
+
+def user_input_search(transactions: list, yes_no: bool) -> list:
+    if yes_no:
+        search_value = input("\nИщем транзакции по определенному слову.\nВведите строку для поиска: ")
         sorted_transactions = sort_re(transactions, search_value.upper(), "description")
-        print("Отсортировано!")
-        return (sorted_transactions)
+        return sorted_transactions
     else:
-        return (transactions)
+        return transactions
+
+
+def count_categories(transactions: list) -> list:
+    """Функция подсчета операций по категориям."""
+    counted = [transaction['description'] for transaction in transactions]
+    qty_operation = len(counted)
+    return (qty_operation)
+
+
+def result_output(transactions: list) -> list:
+    result = []
+    for item in transactions:
+        date = get_date(item["date"])
+        decription = item["description"]
+        result = f"{date}  {decription}\n"
+        # if decription == "Открытие вклада":
+        #     return_mask = get_mask_account(item["to"])
+        # elif decription == "Перевод организации"
+        #     return_mask = f"{get_mask_account(item["from"]} -> {get_mask_account})
+    return(result)

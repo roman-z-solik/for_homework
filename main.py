f6d417ea47c1a32624ec56cce609a0e0d6b7faf3
Roman Z <roman-z@inbox.ru>
2025-05-30 15:24:26 +0500

    Make all operation in interface.py and main.py

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
+import logging
 from pathlib import Path
-from src.interface import select_file_type, sort, sort_re
-from src.interface import user_input_state, user_input_y_n, user_input_search, count_categories, result_output
+from src.interface import import_data, user_input_y_n, input_sort_by_state, sort_re
+from src.interface import sort, count_categories, print_result
 
 root_path = Path(__file__).resolve().parents[1]
+logging.basicConfig(
+    filename=f"{root_path}/logs/interface.log",
+    encoding="utf-8",
+    filemode="w",
+    level=logging.DEBUG,
+    format="%(asctime)s - %(filename)s - %(levelname)s: %(message)s",
+)
+logger_util = logging.getLogger()
 
 if __name__ == "__main__":
     print("Привет! Добро пожаловать в программу работы с банковскими транзакциями.")
-    transact = select_file_type()
-    print(
-        "Введите статус, по которому необходимо выполнить фильтрацию."
-        "\nДоступные для фильтровки статусы: [E]XECUTED, [C]ANCELED, [P]ENDING.\n"
-    )
-    user_state = user_input_state()
+    transact = import_data()
+    user_state = input_sort_by_state()
     transact = sort_re(transact, user_state, 'state')
-    print("Отсортировать операции по дате? [Д]а/[Н]ет\n")
-    yes_no = user_input_y_n()
-    print("\nОтсортировать по возрастанию или по убыванию? По убыванию - [Д]а/По возрастанию - [Н]ет\n")
-    user_reverse = user_input_y_n()
-    transact = sort(transact, "date", user_reverse, yes_no)
-    print("\nВыводить только рублевые транзакции? [Д]а/[Н]ет\n")
+    # for trans in transact:
+    #     print(trans)
+    print("Отсортировать операции по дате? [Д]а/[Н]ет")
+    sort_date = user_input_y_n()
+    if sort_date:
+        print("Отсортировать по возрастанию или по убыванию? По убыванию - [Д]а/По возрастанию - [Н]ет")
+        direction = user_input_y_n()
+        transact = sort (transact, direction, "date")
+    print("\nВыводить только рублевые транзакции? [Д]а/[Н]ет")
     yes_no = user_input_y_n()
     if yes_no:
         transact = sort_re(transact, "RUB", "currency_code")
-    print("\nОтфильтровать список транзакций по определенному слову в описании? [Д]а/[Н]ет\n")
+    print("\nОтфильтровать список транзакций по определенному слову в описании? [Д]а/[Н]ет")
     yes_no = user_input_y_n()
-    transact = user_input_search(transact, yes_no)
-    print("\nРаспечатываю итоговый список транзакций...\n")
-    count = count_categories(transact)
-    if count == 0:
-        print("\nНе найдено ни одной транзакции, \nподходящей под ваши условия фильтрации")
-    else:
-        print(f"Всего банковских операций в выборке: {count}\n")
-        result = result_output(transact)
-    for item in result:
-        if item["description"] == "Открытие вклада":
-            print(f"{item["date"]} {item["description"]}\n{item["from"]}{item["to"]}")
-        else:
-            print(f"{item["date"]} {item["description"]}\n{item["from"]} -> {item["to"]}")
+    if yes_no:
+        search_value = input("Ищем транзакции по определенному слову.\nВведите строку для поиска: ")
+        transact = sort_re(transact, search_value, "description")
+    print_result(transact)
--- src/interface.py
+++ src/interface.py
@@ -16,83 +16,6 @@ logging.basicConfig(
 logger_util = logging.getLogger()
 
 
-def select_file_type() -> list:
-    """Функция предоставляет выбор пользователю, из какого файла импортировать
-    данные о банковских операциях: JSON, CSV или XLSX и возвращает список транзакций"""
-    text = """
-Выберите необходимый пункт меню:
-1. Получить информацию о транзакциях из JSON-файла
-2. Получить информацию о транзакциях из CSV-файла
-3. Получить информацию о транзакциях из XLSX-файла
-"""
-    while True:
-        type_input = input(f"{text}\nВвод: ")
-        if type_input == "1":
-            print("\nДля обработки выбран JSON файл.\n")
-            json_read_file = read_json_file(f"{root_path}/data/operations.json")
-            return json_read_file
-            break
-        elif type_input == "2":
-            print("\nДля обработки выбран CSV файл.\n")
-            return read_csv_file(f"{root_path}/data/transactions.csv")
-            break
-        elif type_input == "3":
-            print("\nДля обработки выбран Excel файл.\n")
-            return read_excel_file(f"{root_path}/data/transactions_excel.xlsx")
-            break
-        else:
-            print("\nВыбран неверный пункт. Повторите ввод.")
-
-
-def user_input_state() -> str:
-    """Функция принимает ввод от пользователя, обозначающий статус,
-    по которому необходимо выполнить фильтрацию. Доступные для фильтровки
-    статусы: EXECUTED, CANCELED, PENDING"""
-    while True:
-        user_input = input("Ввод: ")
-        if user_input.lower() == "executed" or user_input.lower() == "e" or user_input.lower() == "у":
-            user_state = "EXECUTED"
-            print(f"\nОперации отфильтрованы по статусу {user_state}\n")
-            return user_state
-            break
-        elif user_input.lower() == "canceled" or user_input.lower() == "c" or user_input.lower() == "с":
-            user_state = "CANCELED"
-            print(f"\nОперации отфильтрованы по статусу {user_state}\n")
-            return user_state
-            break
-        elif user_input.lower() == "pending" or user_input.lower() == "p" or user_input.lower() == "з":
-            user_state = "PENDING"
-            print(f"\nОперации отфильтрованы по статусу {user_state}\n")
-            return user_state
-            break
-        else:
-            print(f'\nСтатус операции "{user_input}" недоступен. Попробуйте ввести заново.\n')
-
-
-def sort_re(transactions: list, sorted_value: str, sorted_item: str) -> list:
-    """Функция принимает список словарей с данными о банковских операциях и строку поиска,
-    после чего возвращает список словарей, у которых в описании есть данная строка"""
-    sorted_transactions_state = []
-    for item in transactions:
-        try:
-            if re.findall(str(sorted_value), item[sorted_item], flags=re.IGNORECASE):
-                sorted_transactions_state.append(item)
-        except (TypeError, KeyError) as e:
-            logger_util.info(f"{e} ошибка")
-    return sorted_transactions_state
-
-
-def sort(transactions: list, key_value: str, reverse: bool, status: bool) -> list:
-    """Функция принимает список словарей с данными о банковских операциях, строку для сортировки,
-    строку статуса для реверса сортировки, сортирует список, после чего возвращает
-    список словарей."""
-    if status:
-        sorted_transactions = sorted(transactions, key=lambda transaction: transaction[key_value], reverse=reverse)
-        return sorted_transactions
-    else:
-        return transactions
-
-
 def user_input_y_n() -> bool:
     """Функция, отпределяющая, оветил пользователь ДА или НЕТ на вопрос"""
     while True:
@@ -112,41 +35,163 @@ def user_input_y_n() -> bool:
         ):
             return False
         else:
-            print("\nВведите [Д]а или [Н]ет\n")
+            print("Введите [Д]а или [Н]ет\n")
 
 
-def user_input_search(transactions: list, yes_no: bool) -> list:
-    """Функция поиска определяемой пользователем строки в списке транзакций,
-    и выдает отсортированный список по запросу пользователяuser_input_search"""
-    if yes_no:
-        search_value = input("\nИщем транзакции по определенному слову.\nВведите строку для поиска: ")
-        sorted_transactions = sort_re(transactions, search_value.upper(), "description")
-        return sorted_transactions
-    else:
-        return transactions
+def import_data() -> list:
+    """Функция предоставляет выбор пользователю, из какого файла импортировать
+    данные о банковских операциях: JSON, CSV или XLSX и возвращает список транзакций"""
+    text = """
+    Выберите необходимый пункт меню:
+    1. Получить информацию о транзакциях из JSON-файла
+    2. Получить информацию о транзакциях из CSV-файла
+    3. Получить информацию о транзакциях из XLSX-файла
+    """
+    while True:
+        type_input = input(f"{text}\nВвод: ")
+        if type_input == "1":
+            unsorted_data = read_json_file(f"{root_path}/data/operations.json")
+            print("\nДля обработки выбран JSON файл.\n")
+            data = sort_data_JSON(unsorted_data)
+            return data
+        if type_input == "2":
+            unsorted_data = read_csv_file(f"{root_path}/data/transactions.csv")
+            print("\nДля обработки выбран CSV файл.\n")
+            data = sort_data_csv_xlsx(unsorted_data)
+            return data
+        if type_input == "3":
+            unsorted_data = read_excel_file(f"{root_path}/data/transactions_excel.xlsx")
+            print("\nДля обработки выбран Excel файл.\n")
+            data = sort_data_csv_xlsx(unsorted_data)
+            return data
+        else:
+            print("\nВыбран неверный пункт. Повторите ввод.")
+
+
+def sort_data_JSON(data: list) -> list:
+    """Приводит импортированные из JSON файла данные в нужный вид"""
+    sorted_data = []
+    for item in data:
+        if item != {}:
+            sorted_item = {}
+            sorted_item["id"] = item["id"]
+            sorted_item["state"] = item["state"]
+            sorted_item["date"] = item["date"]
+            sorted_item["amount"] = item["operationAmount"]["amount"]
+            sorted_item["currency_name"] = item["operationAmount"]["currency"]["name"]
+            sorted_item["currency_code"] = item["operationAmount"]["currency"]["code"]
+            sorted_item["description"] = item["description"]
+            try:
+                sorted_item["from"] = mask_account_card(item["from"])
+            except (KeyError, TypeError):
+                sorted_item["from"] = ""
+            try:
+                sorted_item["to"] = mask_account_card(item["to"])
+            except (KeyError, TypeError):
+                continue
+            sorted_data.append(sorted_item)
+        else:
+            continue
+    return sorted_data
+
+
+def sort_data_csv_xlsx(data: list) -> list:
+    """Приводит импортированные из CSV или Excel файла данные в нужный вид"""
+    sorted_data = []
+    for item in data:
+        sorted_item = {}
+        sorted_item["id"] = item["id"]
+        sorted_item["state"] = item["state"]
+        sorted_item["date"] = item["date"]
+        sorted_item["amount"] = item["amount"]
+        if item["currency_name"] == "Ruble":
+            sorted_item["currency_name"] = "руб."
+        else:
+            sorted_item["currency_name"] = item["currency_name"]
+        sorted_item["currency_code"] = item["currency_code"]
+        sorted_item["description"] = item["description"]
+        try:
+            sorted_item["from"] = mask_account_card(item["from"])
+        except (KeyError, TypeError):
+            sorted_item["from"] = ""
+        try:
+            sorted_item["to"] = mask_account_card(item["to"])
+        except (KeyError, TypeError):
+            continue
+        sorted_data.append(sorted_item)
+    return sorted_data
+
+
+def input_sort_by_state() -> str:
+    """Функция принимает ввод от пользователя, обозначающий статус,
+    по которому необходимо выполнить фильтрацию. Доступные для фильтровки
+    статусы: EXECUTED, CANCELED, PENDING"""
+    print(
+        "Введите статус, по которому необходимо выполнить фильтрацию."
+        "\nДоступные для фильтровки статусы: [E]XECUTED, [C]ANCELED, [P]ENDING.\n"
+    )
+    while True:
+        user_input = input("Ввод: ")
+        if user_input.lower() == "executed" or user_input.lower() == "e" or user_input.lower() == "t" or user_input == "у":
+            user_state = "EXECUTED"
+            print(f"\nОперации отфильтрованы по статусу {user_state}\n")
+            return user_state
+            break
+        elif user_input.lower() == "canceled" or user_input.lower() == "c" or user_input.lower() == "с":
+            user_state = "CANCELED"
+            print(f"\nОперации отфильтрованы по статусу {user_state}\n")
+            return user_state
+            break
+        elif user_input.lower() == "pending" or user_input.lower() == "p" or user_input.lower() == "з" or user_input == "h":
+            user_state = "PENDING"
+            print(f"\nОперации отфильтрованы по статусу {user_state}\n")
+            return user_state
+            break
+        else:
+            print(f'\nСтатус операции "{user_input}" недоступен. Попробуйте ввести заново.\n')
+            print(
+                "Введите статус, по которому необходимо выполнить фильтрацию."
+                "\nДоступные для фильтровки статусы: [E]XECUTED, [C]ANCELED, [P]ENDING.\n"
+            )
+
+
+def sort_re(transactions: list, sorted_value: str, sorted_item: str) -> list:
+    """Функция принимает список словарей с данными о банковских операциях и строку поиска,
+    после чего возвращает список словарей, у которых в описании есть данная строка"""
+    sorted_transactions = []
+    for item in transactions:
+        if re.findall(str(sorted_value), item[sorted_item], flags=re.IGNORECASE):
+            sorted_transactions.append(item)
+            # print(sorted_transactions)
+    return sorted_transactions
+
+
+def sort(transactions: list, sort_direction: bool, key_value: str) -> list:
+    """Функция принимает список словарей с данными о банковских операциях, строку для сортировки,
+    строку статуса реверса сортировки, сортирует список, после чего возвращает
+    список словарей."""
+    sorted_transactions = sorted(transactions, key=lambda transaction: transaction[key_value], reverse=sort_direction)
+    return sorted_transactions
 
 
 def count_categories(transactions: list) -> int:
-    """Функция подсчета операций по категориям."""
+    """Функция подсчета кол.операций"""
     counted = [transaction["description"] for transaction in transactions]
     qty_operation = len(counted)
     return qty_operation
 
-
-def result_output(transactions: list) -> list:
-    """Формирование окончательного вывода согласно задания."""
-    result_list: list = []
+def print_result(transactions: list) -> None:
+    print("\nРаспечатываю итоговый список транзакций...\n")
+    count = count_categories(transactions)
+    if count == 0:
+        print("\nНе найдено ни одной транзакции, \nподходящей под ваши условия фильтрации")
+    else:
+        print(f"Всего банковских операций в выборке: {count}\n")
     for item in transactions:
-        result_item = {}
         date = get_date(item["date"])
-        result_item["date"] = date
-        description = item["description"]
-        result_item["description"] = description
-        if description == "Открытие вклада":
-            result_item["to"] = mask_account_card(item["to"])
-            result_item["from"] = ""
+        print(f"{date}  {item["description"]}")
+        if item["from"] == "":
+            print(f"{item["to"]}")
         else:
-            result_item["to"] = mask_account_card(item["to"])
-            result_item["from"] = mask_account_card(item["from"])
-        result_list.append(result_item)
-    return result_list
+            print(f"{item["from"]} -> {item["to"]}")
+        print(f"Сумма: {item["amount"]} {item["currency_name"]}\n")
